7eeb662cb151dd9710530e698b10870b3a54b4b2
Hoggspjot <Andreyid67@gmail.com>
2024-09-25 01:23:12 +0300

    123

---
+++ Stage8.java
@@ -0,0 +1,9 @@
+public class Stage8 {
+    public static void main(String[] args) {
+        char a = 'A';
+        a++;
+        System.out.println(a);
+        char c = (char) 1128;
+        System.out.println(c);
+    }
+}
