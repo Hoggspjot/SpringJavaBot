dd5a9aedbcaabf4c4936a80067bc3a5886f1df90
Hoggspjot <Andreyid67@gmail.com>
2025-06-06 23:03:30 +0300

    add autentification feature

--- MyDirectory/Main.java
+++ MyDirectory/Main.java
@@ -1,2 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+
+
+    }
 }
+
+/*
+
+Каждая новая фича — это отдельная ветка от dev.
+
+git checkout -b feature/название-фичи dev
+Пример:
+
+
+git checkout -b feature/authentication dev
+
+
+
+После завершения работы:
+
+git add .
+git commit -m "Add authentication feature"
+git checkout dev
+git merge feature/authentication
+git push*/
